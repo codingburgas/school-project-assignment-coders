fb84360de3cd3fa58722ae5cf5d324dc07f49d24
VBTodorov22 <VBTodorov22@codingburgas.bg>
2024-04-21 20:44:24 +0100

    Add comments in history

--- coders/historyTest.cpp
+++ coders/historyTest.cpp
@@ -1,13 +1,22 @@
-#include "tests.h"
+#include "tests.h" // Including necessary header files
 #include "subjects.h"
 
+// Function definition for history test
 void historyTest()
 {
+    // Clearing the console screen
     system("cls");
+
+    // Displaying test title
     cout << "This is the history test" << endl;
+
+    // Creating a QuestionBank object for history questions
     QuestionBank historyBank;
+
+    // Initializing score variable
     unsigned short int historyScore = 0;
 
+    // Adding history questions to the question bank
     historyBank.addQuestion("Who was the first President of the United States?", "George Washington");
     historyBank.addQuestion("Which ancient civilization built the pyramids?", "Ancient Egyptians");
     historyBank.addQuestion("Who is known as the 'Father of the Renaissance'?", "Leonardo da Vinci");
@@ -29,17 +38,22 @@ void historyTest()
     historyBank.addQuestion("What was the first ancient civilization to develop writing?", "Sumerians");
     historyBank.addQuestion("Who was the first African-American President of the United States?", "Barack Obama");
 
+    // Loop for asking questions
     for (int i = 0; i < 10; i++)
     {
+        // Asking the first question without clearing the screen
         if (i == 0)
         {
+            // Getting a random question from the question bank
             Question q = historyBank.getRandomQuestion();
             cout << "Question " << i + 1 << ": " << q.getQuestion() << endl;
 
+            // Getting user's answer
             string answer;
             cout << "Enter your answer: ";
             getline(cin, answer);
 
+            // Checking if the answer is correct
             if (answer == q.getAnswer()) {
                 cout << "Correct!" << endl;
                 historyScore++;
@@ -48,15 +62,21 @@ void historyTest()
                 cout << "Incorrect. The correct answer is: " << q.getAnswer() << endl;
             }
             cout << endl;
+
+            // Clearing the screen
             system("cls");
         }
+
+        // Asking subsequent questions
         Question q = historyBank.getRandomQuestion();
         cout << "Question " << i + 1 << ": " << q.getQuestion() << endl;
 
+        // Getting user's answer
         string answer;
         cout << "Enter your answer: ";
         getline(cin, answer);
 
+        // Checking if the answer is correct
         if (answer == q.getAnswer()) {
             cout << "Correct!" << endl;
             historyScore++;
@@ -67,7 +87,8 @@ void historyTest()
         cout << endl;
     }
 
-    string grades[5] = { "poor", "average", "good", "very good", "exellent" };
+    // Assigning grade based on the score
+    string grades[5] = { "poor", "average", "good", "very good", "excellent" };
     string grade;
     if (historyScore < 6)
         grade = grades[0];
@@ -80,8 +101,10 @@ void historyTest()
     else if (historyScore > 8)
         grade = grades[4];
 
-
+    // Displaying the score and grade
     cout << "Your score is: " << historyScore << " which is " << grade << " grade" << endl << endl;
+
+    // Asking if the user wants to do another test
     cout << "Would you like to do another test?" << endl;
     cout << "If yes, then press 'y' or 'Y'. If not, then press 'n' or 'N'." << endl;
     cout << "Enter your answer: ";
@@ -89,9 +112,11 @@ void historyTest()
     cin >> answer;
     if (answer == 'y' || answer == 'Y')
     {
+        // Clearing the screen and going back to the subject selection menu
         system("cls");
         subjects();
     }
     else
+        // Exiting the program if the user does not want to do another test
         exit(0);
 }
